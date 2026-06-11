6b44316582beaeb6efc7822e1e5a7d4e17a9fdf5
Nikita <n.v.biryukov@gmail.com>
2026-06-11 10:25:55 +0200

    fetch products and set to useState

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [products, setProducts] = useState({});
+  fetch("https://fakestoreapi.com/products")
+    .then(async (res) => {
+      if (!res.ok) throw new Error("Something went wrong");
+      const json = await res.json();
+      console.log(json);
+      setProducts(json);
+      return json;
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+
+  useEffect(() => {}, [products]);
+
   return (
     <div>
       <h2>Products</h2>
+      <div></div>
     </div>
   );
 };
